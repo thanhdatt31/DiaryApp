b2c681ece2cbf198e5753e01c4b91c76e6481539
thanhdatt31 <thanhdat2k14@gmail.com>
2021-06-02 16:46:56 +0700

    big update

--- app/src/main/java/com/example/diaryapp/SplashActivity.kt
+++ app/src/main/java/com/example/diaryapp/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.example.diaryapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.PreferenceManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -12,11 +14,15 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val sp  = PreferenceManager.getDefaultSharedPreferences(this@SplashActivity)
+        val test = sp.getBoolean("sync",true)
+        Log.d("datnt", "onCreate: $test")
         GlobalScope.launch(Dispatchers.Main) {
             delay(3000)
             val intent = Intent(this@SplashActivity, AppLockActivity::class.java )
             startActivity(intent)
             finish()
+
         }
     }
 }
